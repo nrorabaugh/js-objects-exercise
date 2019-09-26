6eb6af97378a17b90b2f1b308b075be269afce44
Noah Rorabaugh <noahrorabaugh@Noahs-MacBook-Pro.local>
2019-09-26 16:13:16 -0400

    Finished

--- script.js
+++ script.js
@@ -1,27 +1,67 @@
 let calculator = {
+    value: 0,
+
     add: function(num1, num2) {
+        if(num2 === undefined){
+            num2 = num1
+            num1 = this.value
+        }
         let result = num1 + num2
-        return result
+        this.value = result
+        return this.value
     },
+
     subtract: function(num1, num2) {
+        if(num2 === undefined){
+            num2 = num1
+            num1 = this.value
+        }
         let result = num1 - num2
-        return result
+        this.value = result
+        return this.value
     },
+
     multiply: function(num1, num2) {
+        if(num2 === undefined){
+            num2 = num1
+            num1 = this.value
+        }
         let result = num1 * num2
-        return result
+        this.value = result
+        return this.value
     },
+
     divide: function(num1, num2) {
+        if(num2 === undefined){
+            num2 = num1
+            num1 = this.value
+        }
         let result = num1 / num2
-        return result
+        this.value = result
+        return this.value
     },
+
     exp: function(num1, num2) {
+        if(num2 === undefined){
+            num2 = num1
+            num1 = this.value
+        }
         let result = Math.pow(num1, num2)
-        return result
+        this.value = result
+        return this.value
     },
-    root: function(num1, num2) {
+
+    rootDimension: function(num1, num2) {
+        if(num2 === undefined){
+            num2 = num1
+            num1 = this.value
+        }
         let invexp = 1/num2
         let result = Math.pow(num1, invexp)
-        return result
+        this.value = result
+        return this.value
+    },
+    clear: function() {
+        this.value = 0
     }
 }
