58db8dace3dfa574239ba826226c3f148bbb8848
Noah Rorabaugh <noahrorabaugh@Noahs-MacBook-Pro.local>
2019-09-26 13:46:23 -0400

    Part 1 commit

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 let calculator = {
-    add: function()
+    add: function(num1, num2) {
+        let result = num1 + num2
+        return result
+    },
+    subtract: function(num1, num2) {
+        let result = num1 - num2
+        return result
+    },
+    multiply: function(num1, num2) {
+        let result = num1 * num2
+        return result
+    },
+    divide: function(num1, num2) {
+        let result = num1 / num2
+        return result
+    },
+    exp: function(num1, num2) {
+        let result = Math.pow(num1, num2)
+        return result
+    },
+    root: function(num1, num2) {
+        let invexp = 1/num2
+        let result = Math.pow(num1, invexp)
+        return result
+    }
 }
